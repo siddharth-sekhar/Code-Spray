517f0925029d96b7acaaf135abf5d87bc83a8665
Siddharth <siddharthsekhar2004@gmail.com>
2025-08-13 17:11:10 +0530

    changes

---
+++ urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for OnlineJudgeProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', other_app.views.Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.http import HttpResponse
+from users import views as user_views
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),               # Login, Dashboard
+    path('problems/', include('problems.urls')),         # Problem list & detail
+    
+    # Direct access to login and dashboard for convenience
+    path('login/', user_views.user_login, name='login_root'),
+    path('dashboard/', user_views.dashboard, name='dashboard_root'),
+
+    path('health/', health_check, name='health_check'),  # Health check endpoint
+    path('', RedirectView.as_view(url='/login/', permanent=False), name='root'),  # Redirect root to login page
+]
